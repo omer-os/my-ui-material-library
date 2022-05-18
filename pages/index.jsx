dce5ffdc21d10df2a1c58a229a8b5818e75fca66
omar chatin <omerchatin19@gmail.com>
2022-05-18 21:20:17 +0300

    .

---
+++ pages/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from './Layout'
+
+export default function index() {
+  return (
+    <div className='h-screen w-screen overflow-hidden bg-slate-900'>
+      <Layout>
+        
+      </Layout>
+    </div>
+  )
+}
